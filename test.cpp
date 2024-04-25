af4b79d6fe82831708149035bb6291541a222136
node8 <purep@qq.com>
2024-04-25 05:36:20 +0000

    加入对比art需要的数据shuffle

--- test.cpp
+++ test.cpp
@@ -1,38 +1,52 @@
 #include "rbtree.h"
 #include <bits/stdc++.h>
+#include <chrono>
+#include <ctime>
 
-const int max_len = 1e6;
-int data[max_len + 10];
+const int max_len = 5000000;
+long long data[max_len + 10];
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	rb_tree<long long> tree;
-//	std::cout << "test_rb_tree: insert" << std::endl;
-	for (auto i = 0; i < max_len; ++i)
-	{
+    auto n = max_len;
+    rb_tree<long long> tree;
+    std::cout << "test_rb_tree: insert" << std::endl;
+
+    for (uint64_t i = 0; i < n; i++)
+    {
+        // dense, sorted
         data[i] = i + 1;
-		auto debug = data[i];
-		tree.insert(debug);
-	}
+    }
+    if (atoi(argv[1]) == 1)
+    {
+        // dense, random
+        std::random_shuffle(data, data + n);
+    }
+    if (atoi(argv[1]) == 2)
+    {
+        // "pseudo-sparse" (the most-significant leaf bit gets lost)
+        for (uint64_t i = 0; i < n; i++)
+            data[i] = (static_cast<uint64_t>(rand()) << 32) | static_cast<uint64_t>(rand());
+    }
 
-//	std::cout << "test_rb_tree: dump" << std::endl;
-//	tree.dump();
-//	std::cout << "test_rb_tree: height" << std::endl;
-//	std::cout << "height: " << tree.height() << std::endl;
+    auto start = std::chrono::system_clock::now();
+    for (auto i = 0; i < max_len; ++i)
+    {
+        tree.insert(data[i]);
+    }
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cout << "Elapsed time for insertion: " << elapsed_seconds.count() << "s\n";
 
-	for (auto i = 0; i < max_len; ++i)
-	{
-		auto debug = data[i];
-		tree.erase(debug);
-//		std::cout << "test_rb_tree: height: " << tree.height() << " erase: " << debug << std::endl;
-//		std::cout << "test_rb_tree: dump" << std::endl;
-//		tree.check_rb_feature_5();
-//		tree.dump();
-	}
+    // std::cout << "Size of tree node: " << sizeof(rb_tree_node<long long>) << " bytes" << std::endl;
+    std::cout << "Total memory used: " << (double)sizeof(rb_tree_node<long long>) * max_len / 1024 / 1024 << " MBs" << std::endl;
 
-	return 0;
+    for (auto i = 0; i < max_len; ++i)
+    {
+        auto debug = data[i];
+        tree.erase(debug);
+        // tree.dump();
+    }
+
+    return 0;
 }
-
-
-
-
